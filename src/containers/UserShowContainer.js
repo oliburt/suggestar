afc4f1b799bd75f9c9462d8832fa0cc7ec4e9b81
oliburt <oliburt@gmail.com>
2019-11-01 11:44:11 +0000

    container for user show

---
+++ src/containers/UserShowContainer.js
@@ -0,0 +1,23 @@
+import React from "react";
+import UserPlaceHolder from "../components/UserPlaceHolder";
+import UserShow from "../components/UserShow";
+
+class UserShowContainer extends React.Component {
+  componentDidMount() {
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.isAuthenticated === false) return this.props.history.push("/");
+  }
+  
+
+  render() {
+    return (
+      <div>
+        {this.props.isAuthenticated ? <UserShow user={this.props.user} />: <UserPlaceHolder />}
+      </div>
+    );
+  }
+}
+
+export default UserShowContainer;
