c7845c6f13c3fd8f0599d54421c8509617960fc2
oliburt <oliburt@gmail.com>
2019-11-01 08:58:50 +0000

    sidebar slide out menu for routes

---
+++ src/components/SideBarMenu.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Sidebar, Menu, Icon } from "semantic-ui-react";
+import {routes} from "../config/routes";
+import { NavLink } from "react-router-dom";
+
+const SideBarMenu = ({ onHide, user, visible }) => {
+  const filteredRoutes = (routes, user) => {
+    return user
+      ? routes.filter(
+          route => route.title !== "Login" && route.title !== "Registration"
+        )
+      : routes.filter(route => route.title !== "Logout" && route.title !== "My Profile");
+  };
+
+  return (
+      <Sidebar
+        as={Menu}
+        animation="overlay"
+        icon="labeled"
+        inverted
+        onHide={() => onHide(false)}
+        vertical
+        visible={visible}
+        width="thin"
+      >
+        {filteredRoutes(routes, user).map(route => (
+          <Menu.Item
+            key={route.path}
+            as={NavLink}
+            to={route.path === '/users/:id' ? `/users/${user.id}`: route.path}
+            exact
+            activeClassName="active"
+            onClick={() => onHide(false)}
+          >
+            <Icon name={route.icon} />
+            {route.title}
+          </Menu.Item>
+        ))}
+      </Sidebar>
+  );
+};
+
+export default SideBarMenu;
