c0855fc8464ddb46cf613ea73d8d683049161847
oliburt <oliburt@gmail.com>
2019-11-01 08:58:34 +0000

    changed navbar to be button for side menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,15 @@
-import React from 'react';
+import React from "react";
 import { Menu } from "semantic-ui-react";
-import {routes} from '../config/routes';
-import { NavLink } from "react-router-dom";
+// import './Navbar.css'
 
-const Navbar = ({user}) => {
-    return (
-        <Menu>
-            {
-                routes.map(route => (
-                    <Menu.Item 
-                        key={route.path}
-                        as={NavLink}
-                        to={route.path}
-                        exact
-                        activeClassName='active'
-                    >
-                        {route.title}
-                    </Menu.Item>
-                ))
-            }
-            {user && <Menu.Item>Welcome {user.full_name}</Menu.Item>}
-        </Menu>
-    );
-}
+const Navbar = ({ user, handleMenuClick }) => {
+  
+  return (
+    <Menu>
+      <Menu.Item icon="bars" onClick={handleMenuClick} />
+      {user ? <Menu.Item position='right'>Welcome {user.full_name}</Menu.Item> : null}
+    </Menu>
+  );
+};
 
 export default Navbar;
