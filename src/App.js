a127fcc9bd8d0b8580bfbe5b6102071f4ef8e4df
oliburt <oliburt@gmail.com>
2019-11-01 08:57:21 +0000

    added navbar, sidebar and login and validate

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 import {routes} from './config/routes';
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import { Message, Container } from 'semantic-ui-react';
 import Navbar from './components/Navbar';
 import API from "./adapters/API";
+import SideBarMenu from './components/SideBarMenu';
 
 
 const App = props => {
   const [user, setUser] = useState(null);
+  const [sideBarVisible, setSideBarVisible] = useState(false);
 
   const login = user => {
     setUser(user)
     props.history.push('/')
   }
+
+  const logout = () => {
+    API.logout()
+    setUser(null)
+    props.history.push('/')
+  }
+
+  const handleMenuClick = () => setSideBarVisible(!sideBarVisible)
 
   useEffect(() => {
     API.validateUser().then(user => user.id && setUser(user))
@@ -24,25 +34,30 @@ const App = props => {
 
   return (
     <div className="App">
-      <Navbar user={user} />
+      <Navbar user={user} handleMenuClick={handleMenuClick}/>
       <Container>
-        {
-          routes.map(route => (
-            <Route
-              key={route.path}
-              exact
-              path={route.path}
-              component={routerProps => route.component ? (
-                <route.component
-                  {...routerProps}
-                  login={login}
-                />
-              ) : (
-                notFoundMessage()
-              )}
-            />
-          ))
-        }
+        <SideBarMenu onHide={setSideBarVisible} visible={sideBarVisible} user={user}/>
+        <Switch>
+          {
+            routes.map(route => (
+              <Route
+                key={route.path}
+                exact={route.exact}
+                path={route.path}
+                component={routerProps => route.component ? (
+                  <route.component
+                    {...routerProps}
+                    login={login}
+                    logout={logout}
+                    user={user}
+                  />
+                ) : (
+                  notFoundMessage()
+                )}
+              />
+            ))
+          }
+        </Switch>
       </Container>
     </div>
   );
