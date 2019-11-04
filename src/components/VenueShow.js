8f4fce22beef99e37bae36089d47b0b58b88339d
oliburt <oliburt@gmail.com>
2019-11-04 11:24:39 +0000

    basic venue show page

---
+++ src/components/VenueShow.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Header, Button } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+
+const VenueShow = ({ name, description, address, listings}) => {
+    
+    
+    return (
+        <div>
+            <Header as='h1'>{name}</Header>
+            <p>Description: {description}</p>
+            <p>Address: {address}</p>
+            <ul>
+                {listings.map(listing => <Link to={`/listings/${listing.id}`}><li>{listing.title}</li></Link>)}
+            </ul>
+            <Link to="/lisings/new"><Button>Add New Listing</Button></Link>
+        </div>
+    );
+}
+
+export default VenueShow
+ 
+
