47c9dd79f36d9e9941803d07333e24cc87d26789
oliburt <oliburt@gmail.com>
2019-11-04 12:37:58 +0000

    basic form structure without date time

--- src/components/NewListingForm.js
+++ src/components/NewListingForm.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, {useState} from "react";
+import { Form, Button } from "semantic-ui-react";
 
 const NewListingForm = () => {
-    return (
-        <div>
-            listing form
-        </div>
-    );
-}
+    const [title, setTitle] = useState(null);
+    const [description, setDescription] = useState(null);
+    const [ticketURL, setTicketURL] = useState(null);
+    const [ageRestriction, setAgeRestriction] = useState(null);
+
+  return (
+    <Form onSubmit={() => {}}>
+      <Form.Input
+        placeholder="title..."
+        value={title}
+        name="name"
+        onChange={e => setTitle(e.target.value)}
+      />
+      <Form.TextArea
+        placeholder="description..."
+        value={description}
+        name="description"
+        onChange={e => setDescription(e.target.value)}
+      />
+      <Form.Input
+        placeholder="ticket url (if necessary)..."
+        value={ticketURL}
+        name="ticketURL"
+        onChange={e => setTicketURL(e.target.value)}
+      />
+      <Form.Input
+        placeholder="age restriction...?"
+        value={ageRestriction}
+        name="ageRestriction"
+        onChange={e => setAgeRestriction(e.target.value)}
+      />
+
+      <Button type="submit">Create Venue</Button>
+    </Form>
+  );
+};
 
 export default NewListingForm;
+
+//     t.datetime "datetime"
