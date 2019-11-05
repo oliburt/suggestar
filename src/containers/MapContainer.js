5fb2ec23693566a581896f73e5667a02570f7414
oliburt <oliburt@gmail.com>
2019-11-05 13:45:10 +0000

    container for map

---
+++ src/containers/MapContainer.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import ShowMap from "../components/ShowMap";
+
+export class MapContainer extends Component {
+  render() {
+    const { listings, location } = this.props;
+    return (
+      <div style={{width: '90vw', height: '90vh', maxWidth: '1127px'}}>
+        { location[0] && location[1] ? 
+        <ShowMap
+          googleMapUrl={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places`}
+          loadingElement={<div style={{ height: "100%" }} />}
+          containerElement={<div style={{ height: "100%" }} />}
+          mapElement={<div style={{ height: "100%" }} />}
+          location={location}
+          listings={listings}
+        /> : 
+        
+        <div>Loading</div>
+        }
+        
+      </div>
+    );
+  }
+}
+
+export default MapContainer;
