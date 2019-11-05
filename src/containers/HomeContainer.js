78bf2831062ead883e6b7708c843ba8eeb85350a
oliburt <oliburt@gmail.com>
2019-11-05 18:00:45 +0000

    added filter component and change filter

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,23 +1,40 @@
-import React, { Component } from 'react';
-import Home from '../components/Home';
+import React, { Component } from "react";
+import Home from "../components/Home";
+import { Link } from "react-router-dom";
+import HomeFilterForm from "../components/HomeFilterForm";
 
 export class HomeContainer extends Component {
-    
-    filterListings = listings => {
-        return listings
-    }
-    
-    render() {
-        const {listings, location} = this.props
-        const filteredListings = this.filterListings(listings)
-        return (
-            <div>
-                <Home listings={filteredListings} location={location}/>
-            </div>
-        );
-    }
+  state = {
+      options: {
+          filter: 'All'
+      }
+  }
+
+  changeFilter = newFilter => this.setState({options: {
+      ...this.state.options,
+      filter: newFilter
+  }})
+  
+  filterListings = (listings, filter) => {
+      if (filter === "All") return listings;
+      return listings.filter(listing => {
+          return listing.categories.find(cat => cat.name === filter)
+      })
+  };
+
+  render() {
+    const { listings, location } = this.props;
+    const filteredListings = this.filterListings(listings, this.state.options.filter);
+    return listings.length > 0 ? (
+      <div>
+        <HomeFilterForm listings={listings} changeFilter={this.changeFilter} filter={this.state.options.filter}/>
+        <Link to={"/map"}>>>View Map</Link>
+        <Home listings={filteredListings} location={location} />
+      </div>
+    ) : (
+      <div>No Listings...</div>
+    );
+  }
 }
 
 export default HomeContainer;
-
-
