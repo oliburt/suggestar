a83366e024b0eda1dd2a25010cb4e88fad4643a5
oliburt <oliburt@gmail.com>
2019-11-05 13:44:32 +0000

    main container

---
+++ src/containers/MainContainer.js
@@ -0,0 +1,99 @@
+import React, { Component } from "react";
+import { allRoutes } from "../config/routes";
+import { Route, Switch } from "react-router-dom";
+import { Message, Container } from "semantic-ui-react";
+import Navbar from "../components/Navbar";
+import SideBarMenu from "../components/SideBarMenu";
+import API from "../adapters/API";
+
+class MainContainer extends Component {
+  state = {
+    sideBarVisible: false,
+    location: [],
+    listings: [],
+    selectedVenue: null
+  };
+
+  componentDidMount() {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(position => {
+        this.setState({
+          location: [position.coords.latitude, position.coords.longitude]
+        });
+        API.getNearbyListings(
+          position.coords.latitude,
+          position.coords.longitude,
+          2000
+        ).then(listings => {
+          if (listings && listings[0].errors) {
+            console.log("errors:", listings[0].errors);
+          } else {
+            this.setState({ listings });
+          }
+        });
+      });
+    } else {
+      console.log("geolocation not available");
+    }
+  }
+
+  setSideBarVisible = val => this.setState({ sideBarVisible: val });
+
+  handleMenuClick = () => this.setSideBarVisible(!this.state.sideBarVisible);
+
+  notFoundMessage = () => <Message negative>Not Found</Message>;
+
+  render() {
+    const {
+      user,
+      login,
+      logout,
+      isAuthenticated,
+      updateUser,
+      addVenueToCurrentUser,
+      setIsAuthenticated
+    } = this.props;
+    return (
+      <div>
+        <Navbar user={user} handleMenuClick={this.handleMenuClick} />
+        <Container>
+          <SideBarMenu
+            onHide={this.setSideBarVisible}
+            visible={this.state.sideBarVisible}
+            user={user}
+          />
+          <Switch>
+            {allRoutes.map(route => (
+              <Route
+                key={route.path}
+                exact={route.exact}
+                path={route.path}
+                component={routerProps =>
+                  route.component ? (
+                    <route.component
+                      {...routerProps}
+                      login={login}
+                      logout={logout}
+                      user={user}
+                      isAuthenticated={isAuthenticated}
+                      setIsAuthenticated={setIsAuthenticated}
+                      updateUser={updateUser}
+                      addVenueToCurrentUser={addVenueToCurrentUser}
+                      listings={this.state.listings}
+                      location={this.state.location}
+                      selectedVenue={this.state.selectedVenue}
+                    />
+                  ) : (
+                    this.notFoundMessage()
+                  )
+                }
+              />
+            ))}
+          </Switch>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default MainContainer;
