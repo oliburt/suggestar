d3c0b3edf156e06f0d6b1984cbae7a6b3f09ae13
oliburt <oliburt@gmail.com>
2019-11-05 13:44:05 +0000

    extracted api calls for listings and geolocation into maincontainer

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,39 +1,18 @@
 import React, { Component } from 'react';
-import API from '../adapters/API';
 import Home from '../components/Home';
 
 export class HomeContainer extends Component {
-    state = {
-        listings: [],
-        location: []
-    }
-
-    componentDidMount() {
-        if ('geolocation' in navigator) {
-            navigator.geolocation.getCurrentPosition(position => {
-                this.setState({location: [position.coords.latitude, position.coords.longitude]})
-                API.getNearbyListings(position.coords.latitude, position.coords.longitude, 2000).then(listings => {
-                    if (listings && listings[0].errors) {
-                        console.log('errors:', listings[0].errors)
-                    } else {
-                        this.setState({listings})
-                    }
-                })
-            })
-        } else {
-            console.log('geolocation not available')
-        }
-    }
     
     filterListings = listings => {
         return listings
     }
     
     render() {
-        const filteredListings = this.filterListings(this.state.listings)
+        const {listings, location} = this.props
+        const filteredListings = this.filterListings(listings)
         return (
             <div>
-                <Home listings={filteredListings} location={this.state.location}/>
+                <Home listings={filteredListings} location={location}/>
             </div>
         );
     }
