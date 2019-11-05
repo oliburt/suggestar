290a604260a7a3f8bc492fb79693f9c2c9bdc51e
oliburt <oliburt@gmail.com>
2019-11-05 15:49:53 +0000

    started playing with selecting and deselecting listings

--- src/components/ShowMap.js
+++ src/components/ShowMap.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import {
   GoogleMap,
   withGoogleMap,
@@ -8,45 +9,60 @@ import {
 import { Header } from "semantic-ui-react";
 import mapStyles from "../styles/mapStyles";
 
-const ShowMap = withGoogleMap(({ listings, location, selectedListingId }) => {
-  const [selectedListing, setSelectedListing] = useState(
-    listings.find(l => l.id === selectedListingId)
-  );
+const ShowMap = withGoogleMap(
+  ({ listings, location, selectedListingId, setSelectedListingId }) => {
+    const [selectedListing, setSelectedListing] = useState(
+      selectedListingId === -1 ? null : listings.find(l => l.id === selectedListingId)
+    );
 
-  return (
-    <GoogleMap
-      defaultZoom={14}
-      defaultCenter={{ lat: location[0], lng: location[1] }}
-      defaultOptions={{ styles: mapStyles }}
-    >
-      <Marker position={{ lat: location[0], lng: location[1] }} icon={{ url: "/user-location-marker.png", scaledSize: new window.google.maps.Size(30,30)}} />
-      {listings.map(listing => (
+    return (
+      <GoogleMap
+        defaultZoom={14}
+        defaultCenter={{ lat: location[0], lng: location[1] }}
+        defaultOptions={{ styles: mapStyles }}
+      >
         <Marker
-          key={listing.id}
-          position={{
-            lat: listing.venue.latitude,
-            lng: listing.venue.longitude
+          position={{ lat: location[0], lng: location[1] }}
+          icon={{
+            url: "/user-location-marker.png",
+            scaledSize: new window.google.maps.Size(30, 30)
           }}
-          onClick={() => setSelectedListing(listing)}
         />
-      ))}
-      {selectedListing && (
-        <InfoWindow
-          position={{
-            lat: selectedListing.venue.latitude,
-            lng: selectedListing.venue.longitude
-          }}
-          onCloseClick={() => setSelectedListing(null)}
-        >
-          <div>
-            <Header as="h2">{selectedListing.title}</Header>
-            <p>{selectedListing.description}</p>
-            <p>{selectedListing.venue.name}</p>
-          </div>
-        </InfoWindow>
-      )}
-    </GoogleMap>
-  );
-});
+        {listings.map(listing => (
+          <Marker
+            key={listing.id}
+            position={{
+              lat: listing.venue.latitude,
+              lng: listing.venue.longitude
+            }}
+            onClick={() => setSelectedListing(listing)}
+          />
+        ))}
+        {selectedListing && (
+          <InfoWindow
+            position={{
+              lat: selectedListing.venue.latitude,
+              lng: selectedListing.venue.longitude
+            }}
+            onCloseClick={() => {
+              setSelectedListing(null);
+              setSelectedListingId(-1);
+            }}
+          >
+            <div>
+              <Header as="h2">{selectedListing.title}</Header>
+              <Link to={`/listings/${selectedListing.id}`}>>>View Details</Link>
+
+              <p>{selectedListing.description}</p>
+              <Link to={`/venues/${selectedListing.venue.id}`}>
+                {selectedListing.venue.name}
+              </Link>
+            </div>
+          </InfoWindow>
+        )}
+      </GoogleMap>
+    );
+  }
+);
 
 export default ShowMap;
