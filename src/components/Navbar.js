c017c38f747491e4361bd65ca5afadbdee9888b3
oliburt <oliburt@gmail.com>
2019-11-05 11:21:50 +0000

    added onClick history push to home

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Menu } from "semantic-ui-react";
+import { useHistory } from "react-router-dom";
 // import './Navbar.css'
 
 const Navbar = ({ user, handleMenuClick }) => {
-  
+  const history = useHistory()
+
   return (
     <Menu>
       <Menu.Item icon="bars" onClick={handleMenuClick} />
-      {user ? <Menu.Item position='right'>Welcome {user.full_name}</Menu.Item> : null}
+      {user ? <Menu.Item position='right' onClick={() => history.push('/')}>Welcome {user.full_name}</Menu.Item> : null}
     </Menu>
   );
 };
