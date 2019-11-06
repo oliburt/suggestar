2f04924717af15f3e5c0b9f9e0d0dddff3c167d4
oliburt <oliburt@gmail.com>
2019-11-06 11:16:57 +0000

    moved getdistance to helper function file

---
+++ src/helpers/helperFunctions.js
@@ -0,0 +1,25 @@
+export const getDistance = (listing, location) => {
+  if (!location) return "Calculating...";
+
+  const loc1 = [...location];
+  const loc2 = [listing.venue.latitude, listing.venue.longitude];
+  const rad_per_deg = Math.PI / 180;
+  const rkm = 6371;
+  const rm = rkm * 1000;
+
+  const dlat_rad = (loc2[0] - loc1[0]) * rad_per_deg;
+  const dlon_rad = (loc2[1] - loc1[1]) * rad_per_deg;
+
+  const [lat1_rad] = loc1.map(i => i * rad_per_deg);
+  const [lat2_rad] = loc2.map(i => i * rad_per_deg);
+
+  const a =
+    Math.sin(dlat_rad / 2) ** 2 +
+    Math.cos(lat1_rad) * Math.cos(lat2_rad) * Math.sin(dlon_rad / 2) ** 2;
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  return rm * c;
+};
+
+export default {
+  getDistance
+};
