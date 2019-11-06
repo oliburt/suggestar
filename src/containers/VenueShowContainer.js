3369cd5ae5d7dc3261a7652106dc92408d302feb
oliburt <oliburt@gmail.com>
2019-11-06 11:23:40 +0000

    removed unneccessary getaddress functionality

--- src/containers/VenueShowContainer.js
+++ src/containers/VenueShowContainer.js
@@ -13,9 +13,10 @@ class VenueShowContainer extends React.Component {
     const id = this.props.match.params.id
     API.getVenue(id).then(venue => {
       if (venue && venue.id) {
-        this.setState({venue}, () => {
-          this.getAddress(venue)
-        })
+        this.setState({venue})
+        // , () => {
+        //   this.getAddress(venue)
+        // }
       } else {
         console.log("error")
         console.log("Server response", venue)
@@ -30,27 +31,27 @@ class VenueShowContainer extends React.Component {
     if (this.props.isAuthenticated === false) return this.props.history.push("/");
   }
 
-  getAddress = (venue) => {
-    let map = new window.google.maps.Map(document.getElementById("map"), {
-      center: {lat: venue.latitude, lng: venue.longitude}
-    });
+  // getAddress = (venue) => {
+  //   let map = new window.google.maps.Map(document.getElementById("map"), {
+  //     center: {lat: venue.latitude, lng: venue.longitude}
+  //   });
 
-    let service = new window.google.maps.places.PlacesService(map)
+  //   let service = new window.google.maps.places.PlacesService(map)
     
-    service.getDetails({
-      placeId: venue.place_id
-    },(place, status) => {
-      if (status === window.google.maps.places.PlacesServiceStatus.OK) {
-        this.setState({address: place.formatted_address})
-      }
-    })
-  }
+  //   service.getDetails({
+  //     placeId: venue.place_id
+  //   },(place, status) => {
+  //     if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+  //       this.setState({address: place.formatted_address})
+  //     }
+  //   })
+  // }
   
 
   render() {
     return (
       <div>
-        {this.props.isAuthenticated && this.state.venue ? <VenueShow {...this.state.venue} address={this.state.address}/> : <UserPlaceHolder />}
+        {this.props.isAuthenticated && this.state.venue ? <VenueShow {...this.state.venue} user={this.props.user}/> : <UserPlaceHolder />}
         <div id='map'></div>
       </div>
     );
