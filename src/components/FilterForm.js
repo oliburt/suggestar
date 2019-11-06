40e379934ae1be7c8896bf8f7300067828e42153
oliburt <oliburt@gmail.com>
2019-11-06 13:27:29 +0000

    changed name to filter form

--- src/components/FilterForm.js
+++ src/components/FilterForm.js
@@ -1,7 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { Dropdown } from "semantic-ui-react";
 
-const HomeFilterForm = ({ listings, changeFilter, filter }) => {
+const FilterForm = ({
+  listings,
+  changeFilter,
+  filter,
+  radius,
+  setRadius
+}) => {
   const [categoryOptions, setCategoryOptions] = useState([]);
 
   const getCategories = listings => {
@@ -14,6 +20,8 @@ const HomeFilterForm = ({ listings, changeFilter, filter }) => {
 
   const handleChange = (e, data) => changeFilter(data.value);
 
+  const handleRadiusChange = (e, data) => setRadius(data.value);
+
   useEffect(() => {
     if (listings) {
       setCategoryOptions(
@@ -25,6 +33,34 @@ const HomeFilterForm = ({ listings, changeFilter, filter }) => {
       );
     }
   }, [listings]);
+
+  const radiusOptions = [
+    {
+      key: 1,
+      text: "1km",
+      value: 1000
+    },
+    {
+      key: 2,
+      text: "2km",
+      value: 2000
+    },
+    {
+      key: 3,
+      text: "3km",
+      value: 3000
+    },
+    {
+      key: 4,
+      text: "4km",
+      value: 4000
+    },
+    {
+      key: 5,
+      text: "5km",
+      value: 5000
+    }
+  ]
 
   return (
     <div>
@@ -42,8 +78,17 @@ const HomeFilterForm = ({ listings, changeFilter, filter }) => {
           ...categoryOptions
         ]}
       />
+      <span>Change search radius:</span>
+      <Dropdown
+        
+        onChange={handleRadiusChange}
+        value={radius}
+        options={radiusOptions}
+      />
+     
+      
     </div>
   );
 };
 
-export default HomeFilterForm;
+export default FilterForm;
