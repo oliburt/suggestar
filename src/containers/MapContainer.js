2831bdd7a4d1eb4c6c02b7153ba89b056c6fd390
oliburt <oliburt@gmail.com>
2019-11-06 13:29:36 +0000

    applied filters to map markers

--- src/containers/MapContainer.js
+++ src/containers/MapContainer.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 import ShowMap from "../components/ShowMap";
+import FilterForm from "../components/FilterForm";
+import { filterByRadius, filterListingsByEvent } from "../helpers/helperFunctions";
+
 
 export class MapContainer extends Component {
   
   render() {
-    const { listings, location, selectedListingId, setSelectedListingId } = this.props;
+    const { listings, location, selectedListingId, setSelectedListingId, radius, filter } = this.props;
+    const radiusFilteredListings = filterByRadius(listings, location, radius)
+    const eventFilteredListings = filterListingsByEvent(radiusFilteredListings, filter);
+
     return (
-      <div style={{width: '90vw', height: '90vh', maxWidth: '1127px'}}>
+      <>
+      <FilterForm listings={listings} changeFilter={this.props.changeFilter} filter={this.props.filter} radius={this.props.radius} setRadius={this.props.setRadius}/>
+      <div style={{width: '90vw', height: '80vh', maxWidth: '1127px'}}>
         { location[0] && location[1] ? 
         <ShowMap
           googleMapUrl={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places`}
@@ -14,7 +22,7 @@ export class MapContainer extends Component {
           containerElement={<div style={{ height: "100%" }} />}
           mapElement={<div style={{ height: "100%" }} />}
           location={location}
-          listings={listings}
+          listings={eventFilteredListings}
           setSelectedListingId={setSelectedListingId}
           selectedListingId={selectedListingId}
         /> : 
@@ -23,6 +31,7 @@ export class MapContainer extends Component {
         }
         
       </div>
+      </>
     );
   }
 }
