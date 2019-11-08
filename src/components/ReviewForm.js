46ff3aaeacbb9f6d41d3b423a19eb9517b4fb2af
oliburt <oliburt@gmail.com>
2019-11-08 11:38:13 +0000

    handleSubmit

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,13 +1,26 @@
 import React, { useState } from "react";
 import StarRatingComponent from "react-star-rating-component";
-import { Form, Button } from "semantic-ui-react";
+import { Form, Button, Header } from "semantic-ui-react";
+import API from "../adapters/API";
 
-const ReviewForm = () => {
+const ReviewForm = ({venue_id, user_id}) => {
   const [rating, setRating] = useState(0);
   const [content, setContent] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const review = {
+      rating,
+      content,
+      venue_id,
+      user_id 
+    }
+    API.postReview(review).then(console.log)
+  }
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
+      <Header as='h3'>Add a review</Header>
       <StarRatingComponent
         name="rating"
         value={rating}
@@ -17,7 +30,7 @@ const ReviewForm = () => {
         emptyStarColor={"#333"}
         editing={true}
       />
-      <Form.TextArea value={content} onChange={(e,data) => setContent(data.value)}/>
+      <Form.TextArea value={content} onChange={(e,data) => setContent(data.value)} placeholder="Content..."/>
           <Button type='submit'>Submit</Button>
     </Form>
   );
