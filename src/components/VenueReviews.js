5c2ad5d9488be603d9bc0b52c5b916b779a281c9
oliburt <oliburt@gmail.com>
2019-11-10 10:49:48 +0000

    editreview

--- src/components/VenueReviews.js
+++ src/components/VenueReviews.js
@@ -1,20 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import ReviewForm from "./ReviewForm";
 import { Header } from "semantic-ui-react";
 import ReviewListContainer from "../containers/ReviewListContainer";
 
-const VenueReviews = ({id, user, addReview, updateReview, reviews}) => {
+const VenueReviews = ({ id, user, addReview, updateReview, reviews }) => {
+  const [reviewToEdit, setReviewToEdit] = useState(null);
+  
+  const getReview = (reviews, id) => reviews.find(r => r.id === id)
+
   return (
     <>
-      {user ? <ReviewForm
-        venue_id={id}
-        user_id={user.id}
-        addReview={addReview}
-        updateReview={updateReview}
-      /> : null}
+      {user && reviewToEdit ? (
+        <ReviewForm
+          venue_id={id}
+          user_id={user.id}
+          updateReview={updateReview}
+          id={getReview(reviews, reviewToEdit).id}
+          ratingToEdit={getReview(reviews, reviewToEdit).rating}
+          contentToEdit={getReview(reviews, reviewToEdit).content}
+        />
+      ) : user ? (
+        <ReviewForm venue_id={id} user_id={user.id} addReview={addReview} />
+      ) : (
+        <span style={{ fontSize: "10px" }}>Sign in to add a review!</span>
+      )}
 
       {reviews.length > 0 ? (
-        <ReviewListContainer reviews={reviews} />
+        <ReviewListContainer reviews={reviews} setReviewToEdit={setReviewToEdit}/>
       ) : (
         <Header as="h4">No Reviews</Header>
       )}
