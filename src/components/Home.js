32aac3935039e6bda57bc0d28946dafbe55c4967
oliburt <oliburt@gmail.com>
2019-11-10 10:43:04 +0000

    update like on listing

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,10 @@ import React from "react";
 import { Card } from "semantic-ui-react";
 import { renderCards } from "../helpers/helperFunctions";
 
-const Home = ({ listings, location, user, venues }) => {
+const Home = ({ listings, location, user, venues, updateLikeOnListing }) => {
   
   return (
-    <Card.Group centered>{renderCards(listings, location, user, venues)}</Card.Group>
+    <Card.Group centered>{renderCards(listings, location, user, updateLikeOnListing, venues)}</Card.Group>
   )
 };
 
